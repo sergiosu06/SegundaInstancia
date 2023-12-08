fd27ddea73470cd6b9fd512ba9db92fd943b0985
Sergio <sucapucasergio2.com>
2023-12-08 10:57:20 -0400

    subido de controladores

---
+++ proyecto-backend-sergios/Examen/Controllers/EmpresaController.cs
@@ -0,0 +1,49 @@
+﻿using Examen.Context;
+using Examen.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Examen.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmpresaController : Controller
+    {
+        public readonly AplicacionContext _aplicacionContext;
+        public EmpresaController(AplicacionContext context)
+        {
+            _aplicacionContext = context;
+        }
+        [HttpPost]
+        [Route("AgregarEmpresa")]
+        public async Task<IActionResult> Post([FromBody] Empresa empresa)
+        {
+            _aplicacionContext.Empresa.Add(empresa);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Agregado correctamente");
+        }
+        [HttpGet]
+        [Route("MostrarEmpresa")]
+        public async Task<IActionResult> Get()
+        {
+            List<Empresa> listaEmpresa = _aplicacionContext.Empresa.OrderByDescending(e => e.idEmpresa).ToList();
+            return StatusCode(StatusCodes.Status200OK, listaEmpresa);
+        }
+        [HttpPut]
+        [Route("EditarEmpresa/")]
+        public async Task<IActionResult> Put([FromBody] Empresa empresa)
+        {
+            _aplicacionContext.Empresa.Update(empresa);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Editado correctamente ");
+        }
+        [HttpDelete]
+        [Route("EliminarEmpresa/")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            Empresa empresa = _aplicacionContext.Empresa.Find(id);
+            _aplicacionContext.Empresa.Remove(empresa);
+            await _aplicacionContext.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status200OK, "Eliminado correctamente  ");
+        }
+    }
+}
